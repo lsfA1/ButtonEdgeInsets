5645beb31b72e57b36d92699d9ece5f9e75c3e7d
李少锋 <wb59@yingu.com>
2019-06-04 15:02:20 +0800

    设置button上图片和文字上下左右排布

---
+++ CustomButtonEdgeInsets/ButtonEdgeInsets.m
@@ -0,0 +1,21 @@
+//
+//  ButtonEdgeInsets.m
+//  CustomButtonEdgeInsets
+//
+//  Created by 李少锋 on 2019/6/4.
+//  Copyright © 2019年 李少锋. All rights reserved.
+//
+
+#import "ButtonEdgeInsets.h"
+
+@implementation ButtonEdgeInsets
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
